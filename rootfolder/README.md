10e5d46e96fa5d826f7c5dc5ba0db5795d209ff7
ARAVINDSEGAR <47517247+ARAVINDSEGAR@users.noreply.github.com>
2019-11-08 16:21:19 +0530

    Add files via upload

---
+++ rootfolder/README.md
@@ -0,0 +1,26 @@
+# A React Js and Node Js File upload to AWS s3.
+## Installation instructions
+
+in the root directory-
+
+    Install create-react-app globally using below command:
+    npm i -g create-react-app
+
+    npm cache clean --force 
+    npm install
+
+in client directory, execute following commands:
+
+    npm cache clean --force
+    npm install 
+
+ in root directory-
+
+ install the required npm modules:
+
+    npm i aws-sdk multer-s3 multer path url --save
+
+Run the following commands: 
+
+	 npm i -D nodemon
+	 npm run dev
---
+++ rootfolder/routes/api/file.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const multer = require('multer');
+const path = require('path');
+const url = require('url');
+/**
+ * express.Router() - to create modular, mountable route handlers
+ */
+const router = express.Router();
+/**
+ * FILE UPLOADS 
+ */
+const s3 = new aws.S3({
+  accessKeyId: '******************',
+  secretAccessKey: '***************************',
+  Bucket: 'aravindnewbucket1'
+});
+/**
+ * Single Upload
+ */
+const fileUpload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'aravindnewbucket1',
+    acl: 'public-read',
+    key: function (req, file, cb) {
+      cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '-' + Date.now() + path.extname(file.originalname))
+    }
+  }),
+  limits: { fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single('fileImage');
+/**
+ * Check File Type
+ * @param file
+ * @param cb
+ * @return {*}
+ */
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+/**
+ * @route POST api/file/business-img-upload
+ * @desc Upload post image
+ * @access public
+ */
+router.post('/single-file-upload', (req, res) => {
+  fileUpload(req, res, (error) => {
+    console.log('requestOkokok', req.file);
+    console.log('error', error);
+    if (error) {
+      console.log('errors', error);
+      res.json({ error: error });
+    } else {
+      // If File not found
+      if (req.file === undefined) {
+        console.log('Error: No File Selected!');
+        res.json('Error: No File Selected');
+      } else {
+        // If Success
+        const imageName = req.file.key;
+        const imageLocation = req.file.location;
+        // Save the file name into database into file model
+        res.json({
+          image: imageName,
+          location: imageLocation
+        });
+      }
+    }
+  });
+});
+// End of single file upload
+/**
+ * MULTIPLE FILE UPLOADS
+ */
+// Multiple File Uploads ( max 4 )
+const uploadsBusinessGallery = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'aravindnewbucket1',
+    acl: 'public-read',
+    key: function (req, file, cb) {
+      cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '-' + Date.now() + path.extname(file.originalname))
+    }
+  }),
+  limits: { fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).array('galleryImage', 4);
+/**
+ * @route POST /api/file/business-gallery-upload
+ * @desc Upload business Gallery images
+ * @access public
+ */
+router.post('/multiple-file-upload', (req, res) => {
+  uploadsBusinessGallery(req, res, (error) => {
+    console.log('files', req.files);
+    if (error) {
+      console.log('errors', error);
+      res.json({ error: error });
+    } else {
+      // If File not found
+      if (req.files === undefined) {
+        console.log('Error: No File Selected!');
+        res.json('Error: No File Selected');
+      } else {
+        // If Success
+        let fileArray = req.files,
+          fileLocation;
+        const galleryImgLocationArray = [];
+        for (let i = 0; i < fileArray.length; i++) {
+          fileLocation = fileArray[i].location;
+          console.log('filenm', fileLocation);
+          galleryImgLocationArray.push(fileLocation)
+        }
+        // Save the file name into database
+        res.json({
+          filesArray: fileArray,
+          locationArray: galleryImgLocationArray
+        });
+      }
+    }
+  });
+});
+// We export the router so that the server.js file can pick it up
+module.exports = router;
